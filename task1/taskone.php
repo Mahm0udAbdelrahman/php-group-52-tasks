77868b333dc0027eebd99622d43c815381ccca86
Mahm0udAbdelrahman <mahmudabdelrahman0@gmail.com>
2023-09-19 20:22:38 +0300

    taskone

---
+++ task1/taskone.php
@@ -0,0 +1,29 @@
+<?php
+$name="mahmoud";
+echo("Task one");
+echo"<br>";
+echo "Task one1";
+echo"<br>";
+print("Task oneeee");
+echo"<br>";
+echo 20;
+echo"<br>";
+echo "my name is $name";
+echo"<br>";
+#The single makes the variable strange;
+echo 'my name is $name';
+$firstName="Mahmoud";
+$lastName="Abdelrahman";
+$age=22;
+echo"<br>";
+#The difference between single and double with variables;
+echo'My fullname is ' .$firstName .' '. $lastName . 'and my age is ' . $age;
+echo"<br>";
+echo"My fullname is $firstName  $lastName and my age is $age";
+
+
+
+
+
+
+?>
